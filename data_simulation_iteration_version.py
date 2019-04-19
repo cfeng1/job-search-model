16c59d83d007dab4ad2829f2abe6d03d3cf4ab27
Faith Feng <cfeng1@andrew.cmu.edu>
2019-04-19 12:51:19 -0400

    Update data_simulation_iteration_version.py

--- data_simulation_iteration_version.py
+++ data_simulation_iteration_version.py
@@ -1,39 +1,36 @@
 import numpy as np
 import pandas as pd
-<<<<<<< HEAD
-=======
 import functools, itertools
 import os, time, random
->>>>>>> 8c2459bfdaf9bd4e590edd24b2ac25a8c6ceceaa
 
 
-def dataSimulationIteration(successRates, theta, discount, N=3000, T=10):
-    N_sim = 5000
+def dataSimulationIteration(success_rates, theta, discount, N=1000, T=10):
+    N_sim = 2000
     utilityWork = [-np.exp(-theta*x)+0.5 for x in range(0,T)]
     utilityHome = [0]*T
     # no need to allocate space to store
     continuation_value = np.zeros((T+1,T+1))
     epsilon_work = np.random.gumbel(0,1,size = N_sim)
-    epsilon_home = np.random.gumbel(0,1,size = N_sim)
-    success_shock_sim = np.random.random(size = N_sim)   
+    epsilon_home = np.random.gumbel(0,1,size = N_sim)  
 
     for age in range(T-1, -1, -1): 
         for exp in range(age, -1, -1):              
-            success_rate = successRates[exp]
-            success_sim = np.zeros((N_sim,))
-            success_sim[success_shock_sim <= success_rate] = 1
+            success_rate = success_rates[exp]
 
             value_hw = np.zeros((N_sim,2))
             value_hw[:,0] = (utilityHome[exp] + epsilon_home + 
                              discount*continuation_value[age+1,exp])
-            value_hw[:,1] = epsilon_work + success_sim*(
+            value_hw[:,1] = epsilon_work + success_rate*(
                 utilityWork[exp] + discount*continuation_value[age+1,exp+1]) + (
-                1-success_sim)*(utilityHome[exp] + discount*continuation_value[age+1,exp])
+                1-success_rate)*(utilityHome[exp] + discount*continuation_value[age+1,exp])
 
             continuation_value[age,exp] = np.mean(np.max(value_hw,axis=1))
     
     def individualSimulation(i):  
-        nonlocal T, successRates, continuation_value
+        nonlocal T, success_rates, continuation_value
+        if i==1:
+            print(continuation_value)
+
         epsilon_work_i = np.random.gumbel(0,1,size = T)
         epsilon_home_i = np.random.gumbel(0,1,size = T)
         success_shock_sim = np.random.random(size=T)   
@@ -42,7 +39,7 @@ def dataSimulationIteration(successRates, theta, discount, N=3000, T=10):
         choice_i = np.zeros((T,),dtype = int)
         
         for age in range(T):            
-            success_rate = successRates[exp_i[age]]
+            success_rate = success_rates[exp_i[age]]
             value_home = (utilityHome[exp_i[age]] + epsilon_home_i[age] + 
                           discount*continuation_value[age+1,exp_i[age]])
             value_work = (epsilon_work_i[age] + success_rate*(utilityWork[exp_i[age]] + 
@@ -67,7 +64,16 @@ def dataSimulationIteration(successRates, theta, discount, N=3000, T=10):
         
         matrix_sim[i*T:(i+1)*T,:] = individualSimulation(i)
         
-    df_sim = pd.DataFrame(matrix_sim, columns=["individual", "choice", "work_experience", "age"],dtype = int)
+    df_sim = pd.DataFrame(matrix_sim, 
+        columns=["individual", "choice", "work_experience", "age"],dtype = int)
             
     return df_sim
 
+if __name__=="__main__":
+    success = lambda work_experience, T=10: (work_experience/(T-1))*0.2+0.8
+    start = time.time()
+    success_rates = [success(x) for x in range(10)]
+    data = dataSimulationIteration(success_rates, 2, 0.9)
+    data.to_pickle('simulation_search_iteration.pkl')
+    print(time.time()-start)
+    print(data.head())
